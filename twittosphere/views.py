4990d5a54f51c0f85ac82ca0a992bf997722962e
Eric Henry <eric.henry2172@gmail.com>
2016-08-28 16:01:08 -0400

    Added additional methods to BaseView class to flesh out view design

--- twittosphere/views.py
+++ twittosphere/views.py
@@ -1,15 +1,69 @@
+import cherrypy
+
+
 class GenericView(object):
+    """
+    Generic class representing a View with methods to be
+    overridden.
+    """
     def __init__(self, appdir, configs, db, env):
         self._appdir = appdir
         self._configs = configs
         self._db = db
         self._env = env
 
+    def create(self, csrf_token, **kwargs):
+        """
+        Generic view to create an object.
+        """
+        return NotImplementedError()
 
+    @cherrypy.popargs('obj_id')
+    def delete(self, obj_id, csrf_token):
+        """
+        Generic view to delete an object.
+        """
+        return NotImplementedError()
+
+    def _list_view(self):
+        """
+        View to represent all instances of an object type.
+        """
+        return NotImplementedError()
+
+    def _detail_view(self, obj_id):
+        """
+        Detail view of a single object.
+        """
+        return NotImplementedError()
+
+
+@cherrypy.popargs('tweet_id')
 class Tweet(GenericView):
-    pass
+    """
+    Views for Tweets.
+
+    Use as a pattern for other view types. Basic idea is that the
+    web request for /tweets returns a list view.
+
+    If the request matches /tweets/<tweet_id>,
+    a view for a detailed description of that tweet
+    is returned.
+    """
+    @cherrypy.expose
+    def index(self, tweet_id=None):
+        if tweet_id:
+            self._detail_view(tweet_id)
+        self._list_view()
+
+    def _list_view(self):
+        pass
+
+    def _detail_view(self, tweet_id):
+        pass
 
 
+@cherrypy.popargs('project_id')
 class Project(GenericView):
     pass
 
